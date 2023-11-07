83e3a08f9d30d1f3e91a41198698016e82023760
Alejandro Giraldo Quintero <alejogq@gmail.com>
2023-11-07 12:27:59 -0500

    Better rendering

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,18 +1,51 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var options = {
-      chart: {
-        type: 'line'
-      },
-      series: [{
-        name: 'sales',
-        data: [30,40,35,50,49,60,70,91,125]
-      }],
-      xaxis: {
-        categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+  var options = {
+    series: [67],
+    chart: {
+    height: 350,
+    type: 'radialBar',
+    offsetY: -10
+  },
+  plotOptions: {
+    radialBar: {
+      startAngle: -135,
+      endAngle: 135,
+      dataLabels: {
+        name: {
+          fontSize: '16px',
+          color: undefined,
+          offsetY: 120
+        },
+        value: {
+          offsetY: 76,
+          fontSize: '22px',
+          color: undefined,
+          formatter: function (val) {
+            return val + "%";
+          }
+        }
       }
     }
+  },
+  fill: {
+    type: 'gradient',
+    gradient: {
+        shade: 'dark',
+        shadeIntensity: 0.15,
+        inverseColors: false,
+        opacityFrom: 1,
+        opacityTo: 1,
+        stops: [0, 50, 65, 91]
+    },
+  },
+  stroke: {
+    dashArray: 4
+  },
+  labels: ['Median Ratio'],
+  };
 
-    var chart = new ApexCharts(document.querySelector("#chart"), options);
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  chart.render();
 
-    chart.render();
+
 });
